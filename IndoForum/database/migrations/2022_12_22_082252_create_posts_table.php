6735d19b8303ee45714220d6e3bd251aaec2a561
phascolarctostella <tricia.estella@binus.ac.id>
2022-12-22 16:02:31 +0700

    Create model and controller

--- IndoForum/database/migrations/2022_12_22_082252_create_posts_table.php
+++ IndoForum/database/migrations/2022_12_22_082252_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,13 +14,8 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::enableForeignKeyConstraints();
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('role')->default('member');
-            $table->rememberToken();
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
